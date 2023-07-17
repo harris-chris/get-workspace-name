351dd07c362eac090ab4fd189125ab20859f0ebd
harris-chris <chrisharriscjh@gmail.com>
2023-07-17 10:16:15 +0900

    Add overlay, remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,33 +1,32 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
-    flake-utils.url = github:numtide/flake-utils;
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        getworkspacename = pkgs.stdenv.mkDerivation rec {
-          name = "getworkspacename";
-          src = ./getWorkspaceName.cpp;
-          buildInputs = [ pkgs.coreutils ];
-          dontUnpack = true;
-          buildPhase = ''
-            g++ -o getworkspacename $src
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp getworkspacename $out/bin
-            ln -s ${pkgs.wmctrl.out}/bin/wmctrl $out/bin/wmctrl
-            chmod +x $out/bin/getworkspacename
-          '';
-          gcc = pkgs.gcc;
-        };
-      in {
-        defaultPackage = getworkspacename;
-        devShell = pkgs.mkShell {
-          buildInputs = [ getworkspacename ];
-        };
-      });
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      getPackage = pkgs: pkgs.stdenv.mkDerivation rec {
+        name = "getworkspacename";
+        src = ./getWorkspaceName.cpp;
+        buildInputs = [ pkgs.coreutils ];
+        dontUnpack = true;
+        buildPhase = ''
+          g++ -o getworkspacename $src
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp getworkspacename $out/bin
+          ln -s ${pkgs.wmctrl.out}/bin/wmctrl $out/bin/wmctrl
+          chmod +x $out/bin/getworkspacename
+        '';
+        gcc = pkgs.gcc;
+      };
+    in {
+      packages.${system}.default = getPackage pkgs;
+      overlays.default = final: prev: {
+        getworkspacename = getPackage final;
+      };
+    };
 }
 

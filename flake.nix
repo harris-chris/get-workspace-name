793b27a76344a90f4baaa7bc355993a616f9c8aa
harris-chris <chrisharriscjh@gmail.com>
2023-07-18 09:52:33 +0900

    Add overlay

--- flake.nix
+++ flake.nix
@@ -5,28 +5,30 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
+
       pkgs = import nixpkgs { inherit system; };
-      getPackage = pkgs: pkgs.stdenv.mkDerivation rec {
-        name = "getworkspacename";
-        src = ./getWorkspaceName.cpp;
-        buildInputs = [ pkgs.coreutils ];
-        dontUnpack = true;
-        buildPhase = ''
-          g++ -o getworkspacename $src
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp getworkspacename $out/bin
-          ln -s ${pkgs.wmctrl.out}/bin/wmctrl $out/bin/wmctrl
-          chmod +x $out/bin/getworkspacename
-        '';
-        gcc = pkgs.gcc;
-      };
+
+      getPackages = pkgs:
+        let getworkspacename = pkgs.stdenv.mkDerivation rec {
+          name = "getworkspacename";
+          src = ./getWorkspaceName.cpp;
+          buildInputs = [ pkgs.coreutils ];
+          dontUnpack = true;
+          buildPhase = ''
+            g++ -o getworkspacename $src
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp getworkspacename $out/bin
+            ln -s ${pkgs.wmctrl.out}/bin/wmctrl $out/bin/wmctrl
+            chmod +x $out/bin/getworkspacename
+          '';
+          gcc = pkgs.gcc;
+        };
+        in { inherit getworkspacename; };
     in {
-      packages.${system}.default = getPackage pkgs;
-      overlays.default = final: prev: {
-        getworkspacename = getPackage final;
-      };
+      packages.${system} = getPackages pkgs;
+      overlays.default = final: prev: getPackages final;
     };
 }
 
